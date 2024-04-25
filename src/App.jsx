e124453c7b9f2192c3769e9930a84f974b7ec9aa
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-04-26 01:44:31 +0200

    done with tailwind configure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
  
 
   return (
     <>
+
+    <Navbar></Navbar>
     <h1 className=' text-purple-700'>
       Hello From Tailwind
     </h1>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+
+import React from 'react';
+
+const Navbar = () => {
+
+    const routes = [
+        {
+          id: 1,
+          name: "Home",
+          path: "/"
+        },
+        {
+          id: 2,
+          name: "About",
+          path: "/about"
+        },
+        {
+          id: 3,
+          name: "Contact",
+          path: "/contact"
+        },
+        {
+          id: 4,
+          name: "Blog",
+          path: "/blog"
+        },
+        {
+          id: 5,
+          name: "Shop",
+          path: "/shop"
+        }
+      ];
+      
+
+
+    return (
+        <nav>
+            <ul>
+{
+    routes.map(route => <li> {route.name}</li>)
+}
+            </ul>
+            
+        </nav>
+    );
+};
+
+export default Navbar;
