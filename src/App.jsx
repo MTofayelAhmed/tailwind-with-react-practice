d767c6d8ac4887a3ff7edeeff96847f8acdab05a
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-04-26 01:29:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className=' text-purple-700'>
+      Hello From Tailwind
+    </h1>
+    </>
+  )
+}
+
+export default App
